34218af763831337a4e1b54001b6b3dd806d8829
meats88 <pcx883mi@gmail.com>
2026-04-01 20:11:37 -0700

    not done

--- main_read.cpp
+++ main_read.cpp
@@ -25,7 +25,13 @@ cout << "second line 3rd word: " << num << endl;
     // TODO: read M from ifs, print "Total M students"
 
     for (int i = 0; i < M; i++)
-    {
+    {ifs >> sname >> score1 >> score2;
+        sum = score1 + score2;
+        avg = sum / 2.0;
+        r'student name:\s+tom\s+score1:\s+85\s+score2:\s+90\s+sum:\s+175\s+avg:\s+87.5
+        cout << "Student Name: " << sname << ", score1: " <<
+        "score1:" << score1 << " " << "score2:" << score2 << " " << "sum:" << sum << " " << "avg:" << avg << endl;
+
         // TODO: read stuName, score1, score2 from ifs
         // TODO: compute sum = score1 + score2, avg = sum / 2.0
         // TODO: print Student Name, score1, score2, Sum, Avg
